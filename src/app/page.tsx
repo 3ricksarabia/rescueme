ef4ccadcaa98449d193ddac139941df8aa53f8b9
Erick Sarabia <3ricksarabia@gmail.com>
2023-12-26 22:59:47 -0600

    feat(home): get current location to draw a map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
-import { Button } from '@/components/ui/button';
+'use client';
 
-export default function Home() {
+import React, { useEffect, useState, ReactElement } from 'react';
+import { Wrapper } from '@googlemaps/react-wrapper';
+import { Map } from '@/components/ui/map';
+
+const LocationComponent = () => {
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  }, []);
+
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <Wrapper apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}>
+      <Map center={{ lat: latitude, lng: longitude }} zoom={17} />
+    </Wrapper>
   );
-}
+};
+
+export default LocationComponent;
