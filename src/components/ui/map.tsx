1744ef2ea3b71acc247216de8bd68444235b6e77
Erick Sarabia <3ricksarabia@gmail.com>
2023-12-26 22:59:17 -0600

    feat: add map component with a centered marker

---
+++ src/components/ui/map.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { FC, useEffect, useRef } from 'react';
+
+interface MapsProps {
+  center: google.maps.LatLngLiteral;
+  zoom: number;
+}
+
+const Map: FC<MapsProps> = ({ center, zoom }) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      const map = new window.google.maps.Map(ref.current, {
+        center,
+        zoom,
+      });
+
+      const marker = new window.google.maps.Marker({
+        position: center,
+        draggable: true,
+        map,
+      });
+
+      map.addListener('dragend', () => {
+        marker.setPosition(map.getCenter() as google.maps.LatLng);
+      });
+
+      marker.addListener('dragend', () => {
+        map.setCenter(marker.getPosition() as google.maps.LatLng);
+      });
+    }
+  }, [center, ref, zoom]);
+
+  return <div ref={ref} id="map" style={{ width: '100vw', height: '100vh' }} />;
+};
+
+export { Map };
